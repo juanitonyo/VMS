907d2cf43d6e08bac72ac51a93cf6528ac367be5
GlennRoh <glennrossmata078@gmail.com>
2023-03-29 16:41:08 +0800

    Mar29-4:40

--- resources/js/components/Elements/Settings/BuildingTypeTable.vue
+++ resources/js/components/Elements/Settings/BuildingTypeTable.vue
@@ -67,7 +67,7 @@
                     </div>
                     <div class="flex flex-shrink-0 justify-end px-4 py-4 ">
                         <button type="button" class="rounded-md bg-white py-2 px-3 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:ring-gray-400" @click="setOpen">Cancel</button>
-                        <button type="submit" class="ml-4 inline-flex justify-center rounded-md bg-cyan-600 py-2 px-3 text-sm font-semibold text-white shadow-sm hover:bg-cyan-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-cyan-500">{{editMode ? 'Update' : 'Save'}}</button>
+                        <button type="submit" class="ml-4 inline-flex justify-center rounded-md bg-cyan-600 py-2 px-3 text-sm font-semibold text-white shadow-sm hover:bg-cyan-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-cyan-500" >{{editMode ? 'Update' : 'Save'}}</button>
                     </div>
                 </form>
             </template>
--- resources/js/components/Elements/Switch/Toogle.vue
+++ resources/js/components/Elements/Switch/Toogle.vue
@@ -4,7 +4,7 @@
         <SwitchLabel as="span" class="text-sm font-medium leading-6 text-gray-900" passive>{{ title }}</SwitchLabel>
         <SwitchDescription v-show="hasDescription" as="span" class="text-sm text-gray-500">{{ description }}</SwitchDescription>
       </span>
-      <Switch v-model="enabled" :class="[enabled ? 'bg-cyan-600' : 'bg-gray-200', 'relative inline-flex h-6 w-11 flex-shrink-0 cursor-pointer rounded-full border-2 border-transparent transition-colors duration-200 ease-in-out focus:outline-none focus:ring-2 focus:ring-cyan-600 focus:ring-offset-2']">
+      <Switch @click="toggleSwitch" v-model="enabled" :class="[enabled ? 'bg-cyan-600' : 'bg-gray-200', 'relative inline-flex h-6 w-11 flex-shrink-0 cursor-pointer rounded-full border-2 border-transparent transition-colors duration-200 ease-in-out focus:outline-none focus:ring-2 focus:ring-cyan-600 focus:ring-offset-2']">
         <span aria-hidden="true" :class="[enabled ? 'translate-x-5' : 'translate-x-0', 'pointer-events-none inline-block h-5 w-5 transform rounded-full bg-white shadow ring-0 transition duration-200 ease-in-out']" />
       </Switch>
     </SwitchGroup>
@@ -38,8 +38,10 @@ import { Switch, SwitchDescription, SwitchGroup, SwitchLabel } from '@headlessui
             }
         },
         methods:{
-
-        }
+            toggleSwitch() {
+            this.isActive = !this.isActive;
+            this.$emit('switchToggled', this.isActive);
+        }}
     }
 
 </script>
