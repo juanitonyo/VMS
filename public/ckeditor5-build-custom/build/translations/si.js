6968fe2063fb4bbe190c16f49bfeb9eaf235190a
GlennRoh <glennrossmata078@gmail.com>
2023-06-23 08:31:20 +0800

    merged

--- public/ckeditor5-build-custom/build/translations/si.js
+++
@@ -1,1 +0,0 @@
-(function(o){const i=o["si"]=o["si"]||{};i.dictionary=Object.assign(i.dictionary||{},{"%0 of %1":"",Bold:"තදකුරු",Redo:"නැවත කරන්න","Show more items":"",Undo:"අහෝසි කරන්න"});i.getPluralForm=function(o){return o!=1}})(window.CKEDITOR_TRANSLATIONS||(window.CKEDITOR_TRANSLATIONS={}));
